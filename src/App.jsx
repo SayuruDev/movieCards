ddd8410ed6e4cf3ef1474ed622a14f3478a240d7
Sayuru Dev <mailtosayuru@gmail.com>
2025-05-14 04:27:25 -0400

    final push

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
-import './App.css'
-import Home from "./pages/Home"
-import Favorites from "./pages/Favorites"
+import "./css/App.css";
+import Favorites from "./pages/Favorites";
+import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
-import NavBar from './components/NavBar';
+import { MovieProvider } from "./contexts/MovieContext";
+import NavBar from "./components/NavBar";
 
 function App() {
-  const movieNumber = 1;
   return (
-    <div>
+    <MovieProvider>
       <NavBar />
-    < main className ="main-content">
-      < Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/favorites" element={<Favorites />} />
-      </Routes>
-    </main>
-    </div>
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </MovieProvider>
   );
 }
-export default App
+
+export default App;
